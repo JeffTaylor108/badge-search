d9ec08b533c43630ecde04e9a9ecf921eb6445d0
JeffTaylor108 <122399913+JeffTaylor108@users.noreply.github.com>
2023-03-26 19:22:49 -0400

    Update badge-search.js

--- src/badge-search.js
+++ src/badge-search.js
@@ -4,6 +4,7 @@ const logo = new URL('../assets/open-wc-logo.svg', import.meta.url).href;
 
 class BadgeSearch extends LitElement {
   static properties = {
+    
     header: { type: String },
   }
 
