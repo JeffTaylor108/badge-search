7b531f5b44e87b100b1e4969a39bd62d504dc65e
JeffTaylor108 <jeffta1080@gmail.com>
2023-04-08 16:56:20 -0400

    commit

--- src/badge-component.js
+++ src/badge-component.js
@@ -11,7 +11,27 @@ class BadgeComponent extends LitElement {
   }
 
   static styles = css`
-    
+    <a class="tile badge-tile double category-14" id="badge_337" data-badge-id="337" data-model="badge" href="/badges/337/earn">
+
+<div class="badge-top">
+  <span class="pull-left">${this.subject}</span>
+</div>
+
+<div class="tile-body">
+
+  <div class="badge-image pull-right" style="width: 60px; height: 60px; display: block;">
+    <div style="width: 60px; height: 60px; display: block; background-size: contain; background-repeat: no-repeat; background-image: url(/uploads/badge/image/337/APA_Style.png)"></div>
+  </div>
+
+  <h3> ${this.badgeTitle}</h3>
+</div>
+
+<div class="tile-object">
+  <div class="name">
+    Creator: ${this.creator}
+  </div>
+</div>
+</a>
   `;
 
   constructor() {
@@ -25,7 +45,27 @@ class BadgeComponent extends LitElement {
   render() {
 
 return html`
+    <a class="tile badge-tile double category-14" id="badge_337" data-badge-id="337" data-model="badge" href="/badges/337/earn">
 
+<div class="badge-top">
+  <span class="pull-left">${this.subject}</span>
+</div>
+
+<div class="tile-body">
+
+  <div class="badge-image pull-right" style="width: 60px; height: 60px; display: block;">
+    <div style="width: 60px; height: 60px; display: block; background-size: contain; background-repeat: no-repeat; background-image: url(/uploads/badge/image/337/APA_Style.png)"></div>
+  </div>
+
+  <h3> ${this.badgeTitle}</h3>
+</div>
+
+<div class="tile-object">
+  <div class="name">
+    Creator: ${this.creator}
+  </div>
+</div>
+</a>
      `;
   }
 }
